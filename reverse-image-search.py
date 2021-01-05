7b8000f3c0fe769138e6a9fc20a118b0c35391ab
David Singer <david@dsinger.com>
2021-01-04 19:36:49 -0800

    Inital Commit

---
+++ reverse-image-search.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+import requests
+import webbrowser
+import sys
+
+filePath = '/Users/david/Desktop/searchme.jpg'
+try:
+    if sys.argv[1]:
+        filePath = sys.argv[1]
+except IndexError:
+    pass
+
+searchUrl = 'http://www.google.com/searchbyimage/upload'
+multipart = {'encoded_image': (filePath, open(filePath, 'rb')), 'image_content': ''}
+response = requests.post(searchUrl, files=multipart, allow_redirects=False)
+fetchUrl = response.headers['Location']
+webbrowser.open(fetchUrl)
